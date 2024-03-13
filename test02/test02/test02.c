5d603c296d2b7333c6fb20dc0032fbc93300c014
86691 <g86691@gmail.com>
2024-03-13 16:53:22 +0900

    13.last

--- test02/test02/test02.c
+++ test02/test02/test02.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <conio.h>
-int test01();
+void test01();
 void test02(int a);
 void test03();
+void test04();
+void test05();
 void Dump(char* p, int len);
 void Copy(char* p1, char* p2);
 
 int main(void)
 {
+	int n;
+	void* pF[] = { test01,test02,test03,test04,test05 }; // void포인터란 타입이 정해지지않은 포인터 (이 자체로 쓸수는 없음), 사용되기전에 선언적으로 쓸수는 있음, 함수의 주소를 전부 array에 담음
+	void (*pFunc)(); // 함수의 포인터 선언
+	while (1)
+	{
+		printf("\n\n\n\n\n");
+		printf("1, 숫자키에 대한 문자열 출력 미션\n");
+		printf("2, 문자열과 문자배열\n");
+		printf("3, 포인터의 위치 지정\n");
+		printf("4, 포인터를 이용한 문자열 입출력 함수\n");
+		printf("5, 구조체 테스트\n");
+		printf("0, 종 료\n");
+		printf("--------------------------------------\n 선택 ");
+		scanf("%d", &n);
+		if (n == 0) return;
+		pFunc = pF[n - 1];
+		pFunc();
+	}
 	//test01();
 	//test02(1);
-	test03();
+	//test03();
+	//test04();
 }
-int test01()
+void test01() // 숫자키에 대한 문자열 출력 미션
 {
 	char* str[] = { "Zero","One","Two","Three","Four","Five","Six",
 		"Seven","Eight","Nine" }; // 문자열 포인터 배열
@@ -59,6 +80,7 @@ int test01()
 		case 'Q': n = 0; break;
 		}*/
 		//int m = c - 0x30;//ASCII --> num
+		if ((c | 0x20) == 'q') break;
 		printf("%c : %s\n", c, str[c - 0x30]);//m대신 c - 0x30대입 가능
 	}
 }
@@ -89,13 +111,13 @@ void test02(int a) // 문자열과 문자배열
 
 }
 
-void test03()
+void test03() // 포인터의 위치 지정
 {
 	char buf[100];  // 안전 메모리 공간 확보
 	char* pbuf;     // 안전 메모리 공간중의 출력 위치
 	unsigned int addr;       // 출력 위치 지정을 위한 입력변수(주소)
 	char kbuf[100]; // 출력 문자열 입력 공간
-	printf("안전공간의 주소는 %d(%08x) 입니다.\n", (unsigned int)buf, buf);
+	printf("안전공간의 주소는 %u(%08x) 입니다.\n", (unsigned int)buf, buf);
 	printf("입력을 시작할 주소를 입력하세요 : ");
 	scanf("%d", &addr);  // 안전공간 주소 참고
 	pbuf = buf + addr;
@@ -105,6 +127,36 @@ void test03()
 	//strcpy(pbuf, kbuf); // 문자열 복사
 	Dump(buf, 100);
 
+}
+
+void test04() // 포인터를 이용한 문자열 입출력 함수
+{
+	char* arr[10] = { "aaaaa","bbbb","ccc","dd","eeeeee"};
+	char buf[100];
+	printf("[6] "); scanf("%s", buf);
+	arr[6] = buf;
+	printf("[7] "); scanf("%s", buf + 50);
+	arr[7] = buf+50;
+	for (int i = 0; i < 10; i++)
+	{
+		printf("arr[%d] : 0x%08x %s\n", i, arr[i], arr[i]);
+	}
+}
+void test05() // 구조체 테스트
+{
+	struct stTest
+	{
+		int i; //4byte
+		float a;//byte
+		char name[20]; // 8byte
+	} s1 = { 1, 3.14, "삼천갑자 동방삭"};
+	//s1.name = "삼천갑자 동방삭";
+	struct stTest s2 = s1;
+
+	printf("sizeof(struct stTest) : %d\n", sizeof(struct stTest));
+
+	printf("struct stTest s1 : %d %f  %s\n", s1.i, s1.a, s1.name);
+	printf("struct stTest s2 : %d %f  %s\n", s2.i, s2.a, s2.name);
 }
 
 void Copy(char* p1, char* p2)
